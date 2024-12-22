34030e520518867a6cac938586ceed1aba894389
alexZ7000 <78627928+alexZ7000@users.noreply.github.com>
2024-12-21 21:36:09 -0300

    :boom: fix: fixing endpoint error

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import compression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/",
     plugins: [
         react({
             devTarget: "es2022"
