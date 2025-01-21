b2304fa5247eeeeadec1186a992bfbd25363374e
alexZ7000 <alessandrolimafilho@gmail.com>
2025-01-21 17:48:43 -0300

    :recycle: refactor: improving gradient animation

--- src/app/web/screens/Home.tsx
+++ src/app/web/screens/Home.tsx
@@ -11,7 +11,7 @@ export default function Home() {
             <section className={"flex ms-96 pt-16"}>
                 <h1
                     className={
-                        "text-7xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 background-animate bg-[length:200%_100%]"
+                        "text-7xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-bluebright-d via-green-d to-green-d background-animate bg-[length:200%_100%]"
                     }
                 >
                     Unleashing powerful solutions with precision and mastery.
@@ -20,12 +20,12 @@ export default function Home() {
             <section className={"flex justify-center items-center"}>
                 <h1>Work Experience</h1>
             </section>
-            <section
-                className={"flex transition-all justify-center items-center"}
+            <footer
+                className={
+                    "flex flex-col gap-y-12 transition-all justify-center items-center"
+                }
             >
-                <h1>About me</h1>
-            </section>
-            <footer className={"mt-96 pt-96"}>
+                <h1 className={"text-6xl font-bold"}>About me</h1>
                 <Contact />
             </footer>
         </section>
